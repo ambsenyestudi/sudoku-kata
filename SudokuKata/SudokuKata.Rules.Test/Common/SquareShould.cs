a13044c7a969a95e5d4594da598829894a05c735
ambsenyestudi <adrifre7@hotmail.com>
2021-09-12 09:44:38 +0200

    Square valid value

--- SudokuKata/SudokuKata.Rules.Test/Common/SquareShould.cs
+++ SudokuKata/SudokuKata.Rules.Test/Common/SquareShould.cs
@@ -14,5 +14,16 @@ namespace SudokuKata.Rules.Test.Common
             var actual = Square.Create(value);
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(9)]
+        public void CreateFromValue(int value)
+        {
+            var expected = Square.Empty;
+            var actual = Square.Create(value);
+            Assert.NotEqual(expected, actual);
+        }
     }
 }
