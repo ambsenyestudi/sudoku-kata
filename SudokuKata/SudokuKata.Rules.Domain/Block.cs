7cf72c49b46d534239ae0e93becec55980d090fe
ambsenyestudi <adrifre7@hotmail.com>
2021-09-12 09:51:11 +0200

    BlockNotEmpty

--- SudokuKata/SudokuKata.Rules.Domain/Block.cs
+++ SudokuKata/SudokuKata.Rules.Domain/Block.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using SudokuKata.Rules.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SudokuKata.Rules.Domain
 {
     public class Block
     {
+        private readonly List<Square> squares = new List<Square>();
+
         public bool IsEmpty()
         {
-            return true;
+            return !squares.Any();
+        }
+
+        public void Add(Square square)
+        {
+            squares.Add(square);
         }
     }
 }
--- SudokuKata/SudokuKata.Rules.Test/BlockShould.cs
+++ SudokuKata/SudokuKata.Rules.Test/BlockShould.cs
@@ -1,4 +1,5 @@
 using SudokuKata.Rules.Domain;
+using SudokuKata.Rules.Domain.Common;
 using Xunit;
 
 namespace SudokuKata.Rules.Test
@@ -10,6 +11,14 @@ namespace SudokuKata.Rules.Test
         {
             var sut = new Block();
             Assert.True(sut.IsEmpty());
+        }
+        [Fact]
+        public void NotBeEmptyWithOneSquare()
+        {
+            var square = Square.Create(5);
+            var sut = new Block();
+            sut.Add(square);
+            Assert.False(sut.IsEmpty());
         }
     }
 }
