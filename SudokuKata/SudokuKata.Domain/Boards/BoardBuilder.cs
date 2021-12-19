bf8f30e32716ffea2c9e9c1d10c9126d7506d558
ambsenyestudi <adrifre7@hotmail.com>
2021-12-19 10:47:05 +0100

    dotnet 5 update

--- SudokuKata/SudokuKata.Domain/Boards/BoardBuilder.cs
+++ SudokuKata/SudokuKata.Domain/Boards/BoardBuilder.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace SudokuKata
+namespace SudokuKata.Domain.Boards
 {
     public class BoardBuilder
     {
--- SudokuKata/SudokuKata/Game.cs
+++ SudokuKata/SudokuKata/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SudokuKata.Domain.Boards;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
